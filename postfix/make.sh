e50b48da86a5bff0bd7a194fdc7d94b4a070be36
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2022-09-23 09:05:27 +0200

    Update make.sh

--- postfix/make.sh
+++ postfix/make.sh
@@ -2,7 +2,7 @@
 
 # definisco il compilatore da usare
 CC="gcc"
-out="output"
+out="program"
 
 # creo gli object files (".o" uno e zero)
 $CC -c main.c
@@ -11,7 +11,7 @@ $CC -c solvePostfix.c
 $CC -c stack.c
 
 # linko tutti i file oggetto con le librerie necessarie
-$CC main.o infixToPostfix.o solvePostfix.o stack.o -lm -o %out
+$CC main.o infixToPostfix.o solvePostfix.o stack.o -lm -o $out
 
 # elimino tutti i file oggetto
 rm stack.o
